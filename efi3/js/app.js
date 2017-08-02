6c7730854bbca605d361d9e2beff27f319655944
Karol Krusiec <karol@krusiec.com>
2017-08-02 17:21:33 +0200

    added basic validation

--- efi3/js/app.js
+++ efi3/js/app.js
@@ -1,1 +1,18 @@
-$(document).foundation();
+(function() {
+  const password = document.querySelector('.password-field');
+  const submit = document.querySelector('.submit');
+
+  submit.addEventListener('click', e => {
+    e.preventDefault();
+
+    let passValue = password.value;
+
+    console.log(passValue);
+
+    if (passValue.length < 1) {
+      console.log('za mało znakow');
+    }
+
+  })
+
+})();

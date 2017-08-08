9920659f08945baab3aee84f1f348a524182bf92
Karol Krusiec <karol@krusiec.com>
2017-08-08 11:42:17 +0200

    Added login form error handling

--- efi3/js/app.js
+++ efi3/js/app.js
@@ -1,16 +1,35 @@
 (function() {
-  const password = document.querySelector('.password-field');
-  const submit = document.querySelector('.submit');
+  const passContainer = document.querySelector('.password');
+  const passInput = document.querySelector('.password-field');
+  const form = document.querySelector('.form');
+  const errorBox = document.createElement('p');
+  const errorMsg = {
+    'empty': 'To pole nie może być puste',
+    'toShort': 'To pole musi zawierać 5 znaków'
+  }
 
-  submit.addEventListener('click', e => {
+  function addErrorBox() {
+    errorBox.classList.add('error-box');
+    passContainer.after(errorBox);
+  }
+
+  form.addEventListener('click', e => {
     e.preventDefault();
 
-    let passValue = password.value;
-
-    console.log(passValue);
+    let passValue = passInput.value;
 
     if (passValue.length < 1) {
-      console.log('za mało znakow');
+      addErrorBox();
+      passInput.classList.add('error');
+      errorBox.innerHTML = errorMsg.empty;
+    } else if (passValue.length < 5) {
+      addErrorBox();
+      passInput.classList.add('error');
+      errorBox.innerHTML = errorMsg.toShort;
+    } else {
+      passInput.classList.remove('error');
+      document.querySelector('.error-box').remove();
+      window.location.replace('dashboard.html');
     }
 
   })

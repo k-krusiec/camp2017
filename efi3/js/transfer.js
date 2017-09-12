b823489d8836c00b0f25e4017c00d3f27e58f07e
Karol Krusiec <karol@krusiec.com>
2017-09-12 11:01:27 +0200

    added error display

--- efi3/js/transfer.js
+++ efi3/js/transfer.js
@@ -1,5 +1,17 @@
 (function() {
 
+  const findForm = () => {
+    return document.querySelector('form');
+  }
+
+  const findSubmitBtn = () => {
+    return findForm().querySelector('.submit');
+  }
+
+  const findFields = () => {
+    return findForm().querySelectorAll('[data-validation]');
+  }
+
   const errorMsg = {
     empty: 'This field is required',
     accountNumber: {
@@ -18,17 +30,18 @@
     }
   };
 
-  const findForm = () => {
-    return document.querySelector('form');
-  }
-
-  const findSubmitBtn = () => {
-    return findForm().querySelector('.submit');
-  }
-
-  const findFields = () => {
-    return findForm().querySelectorAll('[data-validation]');
-  }
+  const addErrTemplate = (errorData) => {
+    let target = document.querySelector('#' + errorData.target);
+    let errTemplate = `
+      <div class="${errorData.specClass}">
+        <div class="row">
+          <div class="small-9 small-offset-3 columns">
+            <p class="error-sausage">${errorData.msg}</p>
+          </div>
+        </div>
+      </div>`
+    target.insertAdjacentHTML('afterend', errTemplate);
+  };  
 
   const getFormData = () => {
     let fields = {};
@@ -85,17 +98,28 @@
 
   //przerobić zapisywanie klas w obiekcie!
   const validateField = (name, classes, value, valLength) => {
+    const errorBox = document.querySelector('.err-' + name);
     let getData = getFormData();
     let isError = false;
+    let errorData = {
+      target: name,
+      msg: 'error',
+      specClass: 'err-' + name
+    }
+
+    if (errorBox) {
+      errorBox.parentNode.removeChild(errorBox);
+    }
 
     for (var key in getData) {
       let element = document.querySelector('input[name="'+ getData[key].name +'"]');
+
       if (getData[key].name === name) {
         if (!valLength) {
-          console.log(errorMsg.empty);
           element.classList.add('error-border');
+          errorData.msg = errorMsg.empty;
           isError = true;
-          // createErrorBox(errorMsg.empty);
+          addErrTemplate(errorData);
         } else {
           element.classList.remove('error-border');
           isError = false;
@@ -104,30 +128,8 @@
         getData[key].error = isError;
       }
     }
-
     // console.log(getData);
   }
-
-  // const addErrTemplate = (errClass, errMsg, insertTarget) => {
-  //   let errTemplate = `
-  //     <div class="new-error ${errClass}">
-  //       <div class="row">
-  //         <div class="small-9 small-offset-3 columns">
-  //           <p class="dash-error">${errMsg}</p>
-  //         </div>
-  //       </div>
-  //     </div>`
-  //   insertTarget.insertAdjacentHTML('afterend', errTemplate);
-  // };
-  // addErrTemplate(errClass.recipient, errMsg.empty, recipient);
-  //
-  // const createErrorBox = (msg) => {
-  //   var newDiv = document.createElement("div");
-  //   newDiv.classList.add('error-sausage');
-  //   newDiv.innerHTML = msg;
-  //   var my_div = document.querySelector(".search-box");
-  //   my_div.insertAdjacentElement('afterend',newDiv);
-  // }
 
   setEventListener();
 

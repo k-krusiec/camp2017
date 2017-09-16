52c5b3b06a2f8dbd30ef4664bc4263448aeace65
Karol Krusiec <karol@krusiec.com>
2017-09-16 23:18:18 +0200

    added calendar generator

--- efi3/js/calendar.js
+++ efi3/js/calendar.js
@@ -6,6 +6,98 @@ document.addEventListener('DOMContentLoaded', function() {
 var CalendarModule = (function () {
   var options = {};
 
+  var generateCalendar = function (cal, firstDay, numOfDays) {
+    var firstDay = firstDay - 1; // poniedziałek pierwszym dniem tygodnia
+    var numOfWeeks;
+    var remainingDays = numOfDays;
+
+    //jeżeli firstDay to niedziela to 7
+    if (firstDay < 0) {
+      firstDay = 6;
+    }
+
+    //ile tygodni?
+    if (firstDay === 6) { //jeżeli niedziela
+      if (numOfDays === 30 || numOfDays === 31) {
+        numOfWeeks = 6;
+      }
+    } else if (firstDay === 5) { //jeżeli sobota
+      if (numOfDays === 31) {
+        numOfWeeks = 6;
+      }
+    } else if (firstDay === 0) { //jeżeli poniedziałek
+      if (numOfDays === 28) {
+        numOfWeeks = 4;
+      }
+    } else {
+      numOfWeeks = 5;
+    }
+
+    console.log(cal.lastElementChild);
+    // console.log(firstDay);
+    console.log('numOfDays: ', numOfDays);
+
+    for (var i = 0; i < numOfWeeks; i++) {
+      var week = document.createElement('div');
+      week.className = 'cal-week';
+      week.dataset.index = i;
+      cal.lastElementChild.insertAdjacentElement('afterend',week);
+    }
+
+    for (var i = 0; i < 7; i++) { //pierwszy tydzień
+      var otherMonth = document.createElement('div');
+      var normalDay = document.createElement('div');
+      normalDay.innerText = 'normalDay ' + (i+1);
+      normalDay.className = 'cal-day';
+      otherMonth.innerText = 'otherMonth ' + (i+1);
+      otherMonth.classList.add('cal-day');
+      otherMonth.classList.add('last-next-month');
+      if (i < firstDay) {
+        cal.children[1].insertAdjacentElement('beforeend',otherMonth);
+      } else {
+        cal.children[1].insertAdjacentElement('beforeend',normalDay);
+        remainingDays--;
+      }
+    }
+
+    for (var i = 1; i < numOfWeeks - 1; i++) {
+      for (var j = 0; j < 7; j++) { //od drugiego tygodnia to przedostatniego
+        var otherMonth = document.createElement('div');
+        var normalDay = document.createElement('div');
+        normalDay.innerText = 'normalDay ' + (j+1);
+        normalDay.className = 'cal-day';
+        otherMonth.innerText = 'otherMonth ' + (j+1);
+        otherMonth.classList.add('cal-day');
+        otherMonth.classList.add('last-next-month');
+        cal.children[i+1].insertAdjacentElement('beforeend',normalDay);
+        remainingDays--;
+      }
+    }
+
+    for (var i = 0; i < 7; i++) { //ostatni tydzień
+      var otherMonth = document.createElement('div');
+      var normalDay = document.createElement('div');
+      normalDay.innerText = 'normalDay ' + (i+1);
+      normalDay.className = 'cal-day';
+      otherMonth.innerText = 'otherMonth ' + (i+1);
+      otherMonth.classList.add('cal-day');
+      otherMonth.classList.add('last-next-month');
+      if (i < remainingDays) {
+        cal.children[numOfWeeks].insertAdjacentElement('beforeend',normalDay);
+      } else {
+        cal.children[numOfWeeks].insertAdjacentElement('beforeend',otherMonth);
+      }
+    }
+  }
+
+  var firstDayInMonth = function (month, year) {
+    return new Date((month + 1) + ' 1, ' + year).getDay();
+  }
+
+  var daysInMonth = function (month, year) {
+    return new Date(year, month + 1, 0).getDate();
+  }
+
   var setDate = function (inputs, date) {
     var monthsNames = ['January', 'February', 'March', 'April',
                         'May', 'June', 'July', 'August', 'September',
@@ -21,38 +113,42 @@ var CalendarModule = (function () {
     if (index !== 0) {
       if (date.month < 11) {
         date.month++;
-        unchanged = true;
+        wasChange = true;
       } else {
         date.month = 0;
         date.year++;
-        unchanged = true;
+        wasChange = true;
       }
     } else {
       if (date.month > 0) {
         date.month--;
-        unchanged = true;
+        wasChange = true;
       } else {
         date.month = 11;
         date.year--;
-        unchanged = true;
+        wasChange = true;
       }
     }
 
-    if (unchanged) {
+    if (wasChange) {
       setDate(inputs, date);
       return true;
     }
   }
 
-  var dateControlers = function () {
-    var buttons = options.calendar.querySelectorAll('.range a');
+  var dateControllers = function () {
+    var controllers = options.calendar.querySelectorAll('.date-controller');
     var inputs = options.calendar.querySelectorAll('.range span');
+    var cal = options.calendar.querySelector('.calendar-box');
     var today = new Date();
     var date = {
       day : today.getDay(),
       month : today.getMonth(),
       year : today.getFullYear()
     };
+    var firstDay = firstDayInMonth(date.month, date.year);
+    var numOfDays = daysInMonth(date.month, date.year);
+
 
     var forEach = function (array, callback, scope) {
       for (var i = 0; i < array.length; i++) {
@@ -60,16 +156,27 @@ var CalendarModule = (function () {
       }
     };
 
-    forEach(buttons, function(index, button) {
-      if (button.className === 'prev-btn') {
-        button.addEventListener('click', function () {changeDate(index, inputs, date)})
+    forEach(controllers, function(index, controller) {
+      if (index !== 0) {
+        controller.addEventListener('click', function () {
+          changeDate(index, inputs, date);
+          firstDay = firstDayInMonth(date.month, date.year);
+          numOfDays = daysInMonth(date.month, date.year);
+          // generateCalendar(cal, firstDay, numOfDays);
+        });
       } else {
-        button.addEventListener('click', function () {changeDate(index, inputs, date)})
+        controller.addEventListener('click', function () {
+          changeDate(index, inputs, date);
+          firstDay = firstDayInMonth(date.month, date.year);
+          numOfDays = daysInMonth(date.month, date.year);
+          // generateCalendar(cal, firstDay, numOfDays);
+        });
       }
     });
 
     //ustaw dzisiejszą datę na wjazd
     setDate(inputs, date);
+    generateCalendar(cal, firstDay, numOfDays);
   };
 
   var init = function (_options) {
@@ -79,7 +186,7 @@ var CalendarModule = (function () {
       calendar: _options.calendarContainer || null
     };
 
-    dateControlers();
+    dateControllers();
   };
 
   return {
